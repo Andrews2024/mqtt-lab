6ea9012a1bcdadff9bb39f8c7511c185b5bc86b7
Ashe <molly.02.andrews@gmail.com>
2023-02-15 18:00:25 -0500

    Oh boy

---
+++ src/wifi_credentials.h
@@ -0,0 +1,9 @@
+//WiFi credentials
+const char* ssid = "<your ssid>";
+const char* password = "<your network password>";
+
+//MQTT credentials
+const char* mqtt_server = "<mqtt-server-name>";
+#define mqtt_port 1883 // typically 1883
+#define MQTT_USER "<username>"
+#define MQTT_PASSWORD "<password>"
