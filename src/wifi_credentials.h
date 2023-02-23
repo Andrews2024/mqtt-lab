36b3f7fa620328352baae928dee0222f66ce7c00
Ashe <molly.02.andrews@gmail.com>
2023-02-23 13:26:32 -0500

    Removed wifi_credentials.h from cache

--- src/wifi_credentials.h
+++
@@ -1,9 +0,0 @@
-//WiFi credentials
-const char* ssid = "";
-const char* password = "";
-
-//MQTT credentials
-const char* mqtt_server = "";
-#define mqtt_port 1883 // typically 1883
-#define MQTT_USER ""
-#define MQTT_PASSWORD ""
